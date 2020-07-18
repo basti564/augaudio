d9c764255cc053dbe1b65244710fe9e36f6fbe14
Basti564 <bastian.schwickert@gmail.com>
2020-07-18 23:08:06 +0200

    removed generate new data and improved randomaugmentations

--- augaudio/__init__.py
+++ augaudio/__init__.py
@@ -1,6 +1,5 @@
 import librosa as lr
 import numpy as np
-import soundfile
 import random
 
 def GaussianNoise(data, noise_factor):
@@ -27,14 +26,9 @@ def augment(data, augmentation=-1, intensity=1, sampling_rate=None):
         return augmantations[augmentation](data, intensity, sampling_rate).astype(type(data[0]))
     else:
         return augmantations[augmentation](data, intensity).astype(type(data[0]))
-    
-def randomAugmentation(data,sampling_rate):
-    for augmentation in range(0,4):
-        if random.randint(0,1):
-            data=augment(data,augmentation,random.randint(0,10),sampling_rate)
-    return data
 
-def generateNewData(data,sampling_rate,n,path):
-    for count in range(0,n):
-        augData=randomAugmentation(data,sampling_rate)
-        soundfile.write(path+'augmentatedFile'+n+'.wav', augData, sampling_rate)
+def randomAugmentations(data):
+    for augmentation in range(0,len(augmantations)):
+        if bool(random.getrandbits(1)):
+            data = augment(data,augmentation,random.randint(1,5))
+    return data
