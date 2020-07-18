2f2fb2cebd6e312938e02cfe3d3d3903fff00663
Jan Dornhege <jandornhege@gmail.com>
2020-07-18 12:56:14 +0200

    Added randomAugmentation function and function for automaticaly generating a count of new file s

--- augaudio/__init__.py
+++ augaudio/__init__.py
@@ -1,5 +1,7 @@
 import librosa as lr
 import numpy as np
+import soundfile
+import random
 
 def GaussianNoise(data, noise_factor):
     noise = np.random.randn(len(data))
@@ -25,3 +27,14 @@ def augment(data, augmentation=-1, intensity=1, sampling_rate=None):
         return augmantations[augmentation](data, intensity, sampling_rate).astype(type(data[0]))
     else:
         return augmantations[augmentation](data, intensity).astype(type(data[0]))
+    
+def randomAugmentation(data,sampling_rate):
+    for augmentation in range(0,4):
+        if random.randint(0,1):
+            data=augment(data,augmentation,random.randint(0,10),sampling_rate)
+    return data
+
+def generateNewData(data,sampling_rate,n,path):
+    for count in range(0,n):
+        augData=randomAugmentation(data,sampling_rate)
+        soundfile.write(path+'augmentatedFile'+n+'.wav', augData, sampling_rate)
