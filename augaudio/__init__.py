7773f7fd832ae750476e2b5be75fa06e3600f9e9
jandornhege <46827858+jandornhege@users.noreply.github.com>
2020-07-18 23:10:26 +0200

    function for randomly generating combined data augmentations
    * Added randomAugmentation function and function for automaticaly generating a count of new file s
    
    * added install requirement for soundfile
    
    * Revert "added install requirement for soundfile"
    
    This reverts commit c4a1e2306abb75ad9acd4a08a175b89d377ad254.
    
    * removed generate new data and improved randomaugmentations
    
    Co-authored-by: Basti564 <bastian.schwickert@gmail.com>

--- augaudio/__init__.py
+++ augaudio/__init__.py
@@ -1,5 +1,6 @@
 import librosa as lr
 import numpy as np
+import random
 
 def GaussianNoise(data, noise_factor):
     noise = np.random.randn(len(data))
@@ -25,3 +26,9 @@ def augment(data, augmentation=-1, intensity=1, sampling_rate=None):
         return augmantations[augmentation](data, intensity, sampling_rate).astype(type(data[0]))
     else:
         return augmantations[augmentation](data, intensity).astype(type(data[0]))
+
+def randomAugmentations(data):
+    for augmentation in range(0,len(augmantations)):
+        if bool(random.getrandbits(1)):
+            data = augment(data,augmentation,random.randint(1,5))
+    return data
