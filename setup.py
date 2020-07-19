f5f417e6512e6ffb7c16cb831979a40289ce4e0a
basti564 <34898868+BastianOliverSchwickert@users.noreply.github.com>
2020-07-19 19:35:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="augaudio",
-    version="1.0.0",
+    version="1.0.1",
     author="Bastian Schwickert",
     author_email="Bastian.Schwickert@gmail.com",
     description="A simple audio data augmentation package",
